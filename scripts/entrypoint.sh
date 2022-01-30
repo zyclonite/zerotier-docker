78fa865823330ed9c5143278ad72ebd375a41a54
red <red.avtovo@gmail.com>
2022-01-30 12:28:43 +0100

    cleanup bridge

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
-/usr/bin/supervisord --configuration /opt/supervisord.conf &
+PHY_IFACE=eth0
+ZT_IFACE="zt+"
+iptables -t nat -A POSTROUTING -o $PHY_IFACE -j MASQUERADE
+iptables -A FORWARD -i $PHY_IFACE -o $ZT_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $ZT_IFACE -o $PHY_IFACE -j ACCEPT
+
 zerotier-one $@
